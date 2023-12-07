0cded349ca81095ff08c63f72768d7f8780bdc71
Manu-Sh <s3gmentationfault@gmail.com>
2023-12-07 14:57:37 +0100

    wip

--- src/WSS.js
+++ src/WSS.js
@@ -4,7 +4,8 @@ import {createServer} from 'https';
 import {readFileSync} from 'fs';
 import env from './env.js';
 
-export function make_static_https(unset = false) {
+/*
+function make_static_https(unset = false) {
 
     if (unset === true) {
         make_static_https.https_server.close();
@@ -19,22 +20,48 @@ export function make_static_https(unset = false) {
         ca: 'X509 CERTIFICATE',
     });
 }
+*/
 
 export class WSS extends WebSocketServer {
 
-    static https_server;
+    #https_server;
 
     constructor(ws_arg) {
         super(Object.assign({}, {noServer: true, backlog: 100}, ws_arg));
-        this.https_server ??= make_static_https();
+        this.#https_server ??= createServer({
+            minVersion: 'TLSv1',
+            key:  readFileSync(env('WSS_KEY_PEM_PATH')),
+            cert: readFileSync(env('WSS_CRT_PEM_PATH')),
+            ca: 'X509 CERTIFICATE',
+        });
         // this.on('connection', (...args) => this.#setupPurger(...args));
-        this.on('close', () => make_static_https(true)); // TODO: eventualmente implementare un pool
+        this.on('close', () => this.#https_server.close());
     }
 
-    get getHttpsServer() { return this.https_server; }
+    get getHttpsServer() { return this.#https_server; }
 
     listen(...args) {
-        this.https_server.listen(...args);
+        this.#https_server.listen(...args);
+    }
+
+    onHttpsUpgrade(callback_predicate) {
+
+        // https://nodejs.org/docs/latest-v18.x/api/tls.html#class-tlstlssocket
+        this.#https_server.on('upgrade', (request, socket, head) => {
+
+            const result = callback_predicate();
+
+            if (!result) {
+                socket.write('HTTP/1.1 401 Unauthorized\r\n\r\n');
+                socket.destroySoon();
+                return;
+            }
+
+            super.handleUpgrade(request, socket, head, ws => {
+                this.emit('connection', ws, request);
+            });
+
+        });
     }
 
 
@@ -67,25 +94,7 @@ export class WSS extends WebSocketServer {
     }
     */
 
-    onHttpsUpgrade(callback_predicate) {
 
-        // https://nodejs.org/docs/latest-v18.x/api/tls.html#class-tlstlssocket
-        this.https_server.on('upgrade', (request, socket, head) => {
-
-            const result = callback_predicate();
-
-            if (!result) {
-                socket.write('HTTP/1.1 401 Unauthorized\r\n\r\n');
-                socket.destroySoon();
-                return;
-            }
-
-            super.handleUpgrade(request, socket, head, ws => {
-                this.emit('connection', ws, request);
-            });
-
-        });
-    }
 
 
 }
