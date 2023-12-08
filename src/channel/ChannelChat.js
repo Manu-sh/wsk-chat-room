098e81fcaf6f109a93a97a62a77e99ff43493608
Manu-Sh <s3gmentationfault@gmail.com>
2023-12-08 17:35:45 +0100

    channels - wip

--- src/channel/ChannelChat.js
+++ src/channel/ChannelChat.js
@@ -1,10 +1,10 @@
 'use strict';
 import {BasicChat} from '../BasicChat.js'
-import {SimpleChannelsManager} from './ManageChannels.js'
+import {BasicChannelsManager} from './BasicChannelsManager.js'
 
 export class ChannelChat extends BasicChat {
 
-    channels = new SimpleChannelsManager;
+    channels = new BasicChannelsManager;
 
     constructor(...args) {
         super(...args);
